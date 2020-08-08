11d75e9e9e7f94ce88e16275482dec36a9c52e85
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-08 16:45:28 +0200

    Add new component for events list
    (cherry picked from commit 4d05a7149ad96796b444585ef9c0f046210d8a13)

---
+++ app/events/events-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcomming Angular 2 Events</h1>
+    </div>
+    `
+})
+export class EventListComponent {
+    
+}
