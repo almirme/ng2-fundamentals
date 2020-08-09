57f548fba7a60f36d290a44031cf64ab1e6e43a5
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-09 14:14:14 +0200

    Add toastr service

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.service";
+import { ToastrService } from "../common/toastr.service";
 
 @Component({
     selector: 'events-list',
@@ -8,10 +9,15 @@ import { EventService } from "./shared/event.service";
 export class EventListComponent implements OnInit {
     events: any[];
 
-    constructor(private eventService: EventService) {
+    constructor(private eventService: EventService,
+                private toastrService: ToastrService) {
     }
-    
+
     ngOnInit(): void {
         this.events = this.eventService.getEvents();
+    }
+
+    handleClickEvent(eventName) {
+        this.toastrService.success(eventName);
     }
 }
